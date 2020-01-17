0586246882701f6daaa1472b56a785386eb7e97e
Ruan Silva <ruan.silva@widesoftware.com.br>
2020-01-17 10:19:27 -0300

    Configurando Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Header from './components/Header';
 
 import Routes from './routes';
+import store from './store';
 
 export default function App(){
   return(
-    <BrowserRouter>
-      <Header/>
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header/>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
