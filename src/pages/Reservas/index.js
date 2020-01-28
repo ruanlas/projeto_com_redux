a699f524ddbe98ac63c91779cc96de5ba5204b01
Ruan Silva <ruanluiz.as@gmail.com>
2020-01-28 15:58:08 -0300

    Mudando a quantidade

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { MdDelete } from 'react-icons/md';
+import { MdDelete, MdAddCircle, MdRemoveCircle } from 'react-icons/md';
 
-import { removeReserve } from '../../store/modules/reserve/actions';
+import { removeReserve, updateAmountReserve } from '../../store/modules/reserve/actions';
 
 import './style.css';
 
@@ -22,6 +22,18 @@ export default function Reservas() {
         dispatch(action);
     }
 
+    function incrementAmont(reserve){
+        // console.log(reserve);
+        let action = updateAmountReserve(reserve.id, reserve.amount + 1);
+        dispatch(action);
+    }
+
+    function decrementAmont(reserve){
+        // console.log(reserve);
+        let action = updateAmountReserve(reserve.id, reserve.amount - 1);
+        dispatch(action);
+    }
+
     return (
         <div>
             <h1 className="title">Você solicitou {reserves.length} reservas</h1>
@@ -31,7 +43,18 @@ export default function Reservas() {
                     <div className="reservas" key={reserve.id}>
                         <img src={reserve.image} alt={reserve.title} />
                         <strong>{reserve.title}</strong>
-                        <span>Quantidade: {reserve.amount}</span>
+                        
+                        <div id="amount">
+                            <button type="button" onClick={() => decrementAmont(reserve) }>
+                                <MdRemoveCircle size={25} color="#191919"/>
+                            </button>
+                            <input type="text" readOnly value={reserve.amount} />
+                            {/* <span>Quantidade: {reserve.amount}</span> */}
+                            <button type="button" onClick={() => incrementAmont(reserve) }>
+                                <MdAddCircle size={25} color="#191919"/>
+                            </button>
+                        </div>
+
                         <button type="button" onClick={() => { handleRemove(reserve.id) }}>
                             <MdDelete size={20} color="#191919" />
                         </button>
